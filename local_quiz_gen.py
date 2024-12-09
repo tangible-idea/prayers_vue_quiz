c2672927b13e8f97310b817cf62c450927963a51
Mark Choi <fantasysa@gmail.com>
2024-12-10 01:24:54 +0900

    space.

--- local_quiz_gen.py
+++ local_quiz_gen.py
@@ -8,7 +8,7 @@ import streamlit as st
 
 
 async def gen(book_name="창세기", how_many=10, progress_text=None, progress_bar=None):
-    header_query= f"Generate {how_many} of Bible quiz questions for book name: {book_name} in Korean."
+    header_query= f"Generate {how_many} of Bible quiz questions for book name: {book_name} in Korean. "
     query_to_llm = header_query + 'Include question, options, answer, and reference(where the quiz from). Provide a certain format in JSON : "{ "data": [{ "question":"", "difficulty": (number 1-10), options: [...], answer: "", reference: "", type: "bible"]} Do not put any other message besides the JSON format. (even without ```json or ```)'
     print(f"query_to_llm : {query_to_llm}")
 
@@ -67,3 +67,5 @@ if button1_clicked:
 
     progress_text.text("완료!")
     progress_bar.empty()
+
+run(gen())
