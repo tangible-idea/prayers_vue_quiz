e251b102bfb5e4a45c3cb8506282fd3205b9b1ee
Mark Choi <fantasysa@gmail.com>
2024-12-10 22:19:03 +0900

    퀴즈 중복 의심 도우미 추가.

--- llm.py
+++ llm.py
@@ -3,7 +3,7 @@ import os
 
 from supabase_client import POE_KEY
 
-async def send_message(message: str):
+async def gpt4o(message: str):
     api_url = "https://ai.tangibly.link/bot/GPT-4o"
     apikey = POE_KEY
     params = {
@@ -12,7 +12,23 @@ async def send_message(message: str):
     }
     response = requests.get(api_url, params=params)
     try:
-        print(f"response from send_message : {response}")
+        print(f"response from gpt4o : {response}")
+        jsonResponse= response.json()
+        return jsonResponse
+    except Exception as ex:
+        print(f"JSONDecodeError???: {ex}")
+    return {"error": "Error in parsing."}
+
+async def claude35Sonnet200k(message: str):
+    api_url = "https://ai.tangibly.link/call/Claude-3.5-Sonnet-200k"
+    apikey = POE_KEY
+    params = {
+        'request': message,
+        'apikey': apikey
+    }
+    response = requests.post(api_url, json=params)
+    try:
+        print(f"response from Claude-3.5-Sonnet-200k : {response}")
         jsonResponse= response.json()
         return jsonResponse
     except Exception as ex:
--- local_quiz_gen.py
+++ local_quiz_gen.py
@@ -1,4 +1,4 @@
-from llm import send_message
+from llm import gpt4o, claude35Sonnet200k
 from supabase_client import supabase
 from asyncio import run
 import json
@@ -12,7 +12,7 @@ async def gen(book_name="창세기", how_many=10, progress_text=None, progress_b
     query_to_llm = header_query + 'Include question, options, answer, and reference(where the quiz from). Provide a certain format in JSON : "{ "data": [{ "question":"", "difficulty": (number 1-10), options: [...], answer: "", reference: "", type: "bible"]} Do not put any other message besides the JSON format. (even without ```json or ```)'
     print(f"query_to_llm : {query_to_llm}")
 
-    response = await send_message(query_to_llm)
+    response = await gpt4o(query_to_llm)
     from_llm = response.get("message", "Error")
     print("=====")
     print(from_llm)
@@ -46,10 +46,25 @@ async def gen(book_name="창세기", how_many=10, progress_text=None, progress_b
 
 st.header("BQ gen")
 st.subheader("현재 퀴즈DB 조회")
-button2_clicked = st.button("DB 검색하기")
+button2_clicked = st.button("DB 조회")
 if button2_clicked:
-    result = supabase.table('quiz').select('*').execute()
-    st.dataframe(result.data)
+    result = supabase.table('quiz').select('id,question,options,answer,reference,type,difficulty').execute()
+    st.dataframe(result.data, selection_mode="single-row")
+
+button3_redundant_clicked = st.button("중복의심되는 퀴즈 찾기")
+if button3_redundant_clicked:
+    with st.spinner("중복 퀴즈를 검색중입니다..."):
+        result = supabase.table('quiz').select('id,question,options').execute()
+        query_to_llm = f'{result} \n\n 여기에서 유사한 제목의 퀴즈를 찾고, id를 set로 출력해줘. 예시: 중복의심제목: [13,194,222...], 중복의심제목: [3,4...]'
+        print(f"query_to_llm : {query_to_llm}")
+
+        async def check_redundant():
+            from_llm = await claude35Sonnet200k(query_to_llm)
+            print("=====")
+            print(from_llm)
+            st.text_area("중복검출된 퀴즈들", value=from_llm, height=350)
+
+        run(check_redundant())
 
 st.subheader("퀴즈 생성하고 자동업로드")
 
@@ -67,5 +82,3 @@ if button1_clicked:
 
     progress_text.text("완료!")
     progress_bar.empty()
-
-run(gen())
