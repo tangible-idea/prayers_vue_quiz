27ca0f1a738511344323e4d885129b74a9f4f7df
Mark Choi <shinhan.mark@gmail.com>
2025-02-28 11:48:32 +0900

    gen_paintings function to generate painting related quizes.

--- local_quiz_gen.py
+++ local_quiz_gen.py
@@ -10,7 +10,45 @@ import streamlit as st
 
 
 
-async def gen(book_name="창세기", how_many=10, progress_text=None, progress_bar=None):
+# gen quiz related to paintings Bible books
+async def gen_paintings(book_name="창세기", painting_name="unknown", how_many=10, progress_text=None, progress_bar=None):
+    header_query= f"Generate {how_many} of Bible quiz questions for book name: {book_name} in Korean. but it has to be somewhat related to Christian masterpiece paintings, the name of the painting is {painting_name}"
+    query_to_llm = header_query + 'Include question, options, answer, and reference(where the quiz from). Provide a certain format in JSON : "{ "data": [{ "question":"", "difficulty": (number 1-10), options: [...], answer: "", reference: "", type: "bible"]} Do not put any other message besides the JSON format. (even without ```json or ```)'
+    print(f"query_to_llm : {query_to_llm}")
+
+    response = await gpt4o(query_to_llm)
+    from_llm = response.get("message", "Error")
+    print("=====")
+    print(from_llm)
+    if(from_llm == "Error"):
+        st.text_area("오류 발생!", value=from_llm)
+        return
+
+    json_data= json.loads(from_llm)
+    print("=====")
+    print(json_data)
+    print("=====")
+
+    for index, quiz in enumerate(json_data['data']):
+        print(f"{index}: {quiz}")
+        time.sleep(1)
+        progress_text.text(f"{index+1}개 진행중...")
+        progress_bar.progress((index + 1) / how_many)
+
+        result_rpc = supabase.rpc('insert_quiz_painting', {
+                        'p_question': str(quiz['question']),
+                        'p_difficulty': quiz['difficulty'],
+                        'p_options': quiz['options'],
+                        'p_answer': str(quiz['answer']),
+                        'p_type': str(quiz['type']),
+                        'p_reference': str(quiz['reference'])
+                    }).execute()
+        print("result_rpc ====> " + str(result_rpc.data))
+        st.text_area("생성된 퀴즈", value=str(result_rpc.data) + "\n\n" + str(quiz), height=110)
+
+
+# gen quiz only related to classic Bible books
+async def gen_bq(book_name="창세기", how_many=10, progress_text=None, progress_bar=None):
     header_query= f"Generate {how_many} of Bible quiz questions for book name: {book_name} in Korean. "
     query_to_llm = header_query + 'Include question, options, answer, and reference(where the quiz from). Provide a certain format in JSON : "{ "data": [{ "question":"", "difficulty": (number 1-10), options: [...], answer: "", reference: "", type: "bible"]} Do not put any other message besides the JSON format. (even without ```json or ```)'
     print(f"query_to_llm : {query_to_llm}")
@@ -103,7 +141,8 @@ with tab1:
             run(check_redundant())
 
 with tab2:
-    book_name = st.text_input("Book name", placeholder="예: Genesis, Exodus, Leviticus, Numbers, Deuteronomy")
+    book_name = st.text_input("Book name", placeholder="예: Genesis, Exodus, 창세기, 출애굽기 등...")
+    painting_name = st.text_input("명화 이름/설명", placeholder="예: 레오나르도 다빈치의 최후의 만찬, 카라바조의 도마뱀에 물린 소년 등...")
     count = st.text_input("개수", value="2", max_chars=2)
     button1_clicked = st.button(f"{count}개의 퀴즈 만들기")
     if button1_clicked:
@@ -113,7 +152,7 @@ with tab2:
         progress_text.text("퀴즈를 생성중입니다...")
         progress_bar.progress(50)
 
-        run(gen(book_name, int(count), progress_text, progress_bar))
+        run(gen_paintings(book_name, painting_name, int(count), progress_text, progress_bar))
 
         progress_text.text("완료!")
         progress_bar.empty()
