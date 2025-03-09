faa9d45bf4a8fcd3b22e3c15fd1f2774bfcefce5
Mark Choi <fantasysa@gmail.com>
2025-03-09 18:31:09 +0900

    gen_paintings function is only related to paintings,

--- local_quiz_gen.py
+++ local_quiz_gen.py
@@ -11,9 +11,9 @@ import streamlit as st
 
 
 # gen quiz related to paintings Bible books
-async def gen_paintings(book_name="창세기", painting_name="unknown", how_many=10, progress_text=None, progress_bar=None):
-    header_query= f"Generate {how_many} of Bible quiz questions for book name: {book_name} in Korean. but it has to be somewhat related to Christian masterpiece paintings, the name of the painting is {painting_name}"
-    query_to_llm = header_query + 'Include question, options, answer, and reference(where the quiz from). Provide a certain format in JSON : "{ "data": [{ "question":"", "difficulty": (number 1-10), options: [...], answer: "", reference: "", type: "bible"]} Do not put any other message besides the JSON format. (even without ```json or ```)'
+async def gen_paintings(painting_name="unknown", how_many=10, progress_text=None, progress_bar=None):
+    header_query= f"Generate {how_many} of quiz questions for a painting. The name of the painting is {painting_name}"
+    query_to_llm = header_query + 'Include question, options, answer, and reference(what year the painting related of). Provide a certain format in JSON : "{ "data": [{ "question":"", "difficulty": (number 1-10), options: [...], answer: "", reference: "", type: "painting"]} Do not put any other message besides the JSON format. (even without ```json or ```)'
     print(f"query_to_llm : {query_to_llm}")
 
     response = await gpt4o(query_to_llm)
@@ -141,7 +141,7 @@ with tab1:
             run(check_redundant())
 
 with tab2:
-    book_name = st.text_input("Book name", placeholder="예: Genesis, Exodus, 창세기, 출애굽기 등...")
+    #book_name = st.text_input("Book name", placeholder="예: Genesis, Exodus, 창세기, 출애굽기 등...")
     painting_name = st.text_input("명화 이름/설명", placeholder="예: 레오나르도 다빈치의 최후의 만찬, 카라바조의 도마뱀에 물린 소년 등...")
     count = st.text_input("개수", value="2", max_chars=2)
     button1_clicked = st.button(f"{count}개의 퀴즈 만들기")
